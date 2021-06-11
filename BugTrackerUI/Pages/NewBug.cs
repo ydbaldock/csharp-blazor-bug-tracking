fc4d778559c24efd8799e34f3ec6104758a1fb75
Yolanda Baldock <ydbaldock@msn.com>
2021-06-11 15:44:49 -0400

    Completed module

--- BugTrackerUI/Pages/NewBug.cs
+++ BugTrackerUI/Pages/NewBug.cs
@@ -1,7 +1,21 @@
-﻿namespace BugTrackerUI.Pages
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BugTrackerUI.Pages
 {
     public partial class NewBug
     {
         public Bug AddBug => new Bug();
+
+        [Inject]
+        public IBugService BugService { get; set; }
+
+        [Inject]
+        public NavigationManager NavService { get; set; }
+
+        protected void HandleValidSubmit()
+        {
+            BugService.AddBug(AddBug);
+            NavService.NavigateTo("");
+        }
     }
 }

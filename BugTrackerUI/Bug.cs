b72f698019a993dbc484b651d668945d1459a839
Yolanda Baldock <ydbaldock@msn.com>
2021-06-11 16:04:50 -0400

    Completed module

--- BugTrackerUI/Bug.cs
+++ BugTrackerUI/Bug.cs
@@ -1,13 +1,20 @@
-﻿namespace BugTrackerUI
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BugTrackerUI
 {
     public class Bug
     {
         public int Id { get; set; }
 
+        [Required]
         public string Title { get; set; }
 
+        [Required]
+        [MinLength(10)]
         public string Description { get; set; }
 
+        [Required]
+        [Range(1,5)]
         public int Priority { get; set; }
     }
 }
